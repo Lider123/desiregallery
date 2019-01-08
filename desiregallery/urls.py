bcceda9ab7dfbc9279b5156bab9a6e78165fa7fa
Konstantin <k_babaec@mail.ru>
2019-01-08 20:21:34 +0500

    Added redirection to main page

--- desiregallery/urls.py
+++ desiregallery/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="gallery/", permanent=False)),
     path('admin/', admin.site.urls),
     path('gallery/', include("gallery.urls")),
 ]
