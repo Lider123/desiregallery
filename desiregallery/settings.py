2eea4f1743d1b11715313bcc1ca5ddd52fad6e3e
Konstantin <k_babaec@mail.ru>
2019-08-25 22:24:59 +0500

    Migrated to mysql

--- desiregallery/settings.py
+++ desiregallery/settings.py
@@ -79,12 +79,10 @@ WSGI_APPLICATION = 'desiregallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "gallery",
-        "USER": os.environ["DB_USER"],
-        "PASSWORD": os.environ["DB_PASS"],
-        "HOST": "127.0.0.1",
-        "PORT": "5432"
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'desiregallery/my.cnf',
+        },
     }
 }
 
