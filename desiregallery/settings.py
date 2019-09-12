728ee3270aa7c2b42f3a5dba469029387e531833
Konstantin <k_babaec@mail.ru>
2019-09-12 22:19:32 +0500

    Refactored

--- desiregallery/settings.py
+++ desiregallery/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'gallery',
+    'desiregallery.apps.gallery',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
